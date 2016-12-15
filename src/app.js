f4ec7cae2a428fdcc5615feb72d6b730147fafd4
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2016-12-15 17:04:23 -0500

    beginning storage

--- src/app.js
+++ src/app.js
@@ -4,26 +4,32 @@ const a = require('./arguments.js');
 
 const brand = `<h1 class="brandName">${i18nService.t('brand.appName')}</h1>`;
 const welcome = `<p id="welcome" class="welcome">${i18nService.t('begin with a judgment')}</p>`;
-const startArgument = `<div class="startArgument">If <input name="if" class="ifInput" placeholder="x is true">, then <input name="then" class="thenInput" placeholder="y is true">. <button id="submit" class="submitArgument">+</button></div>`;
 
 function init() {
-  const c = document.getElementById(containerId);
-    if(c === null) {
-      console.log(i18nService.t('errors.noRoot'));
-    } else {
-      c.innerHTML += brand;
-      c.innerHTML += welcome;
-      c.innerHTML += startArgument;
-    }
+	a.initStorage();
 
-  const submitArgumentButton = document.getElementById('submit');
-  submitArgumentButton.addEventListener('click', function() {
-    const [i, t] = document.querySelectorAll('input');
-    a.storeArgument(i.value, t.value);
-    // TODO: c.innerHTML += startArgument;
-  });
+	const c = document.getElementById(containerId);
 
-  return document;
+	if(c === null) {
+		console.log(i18nService.t('errors.noRoot'));
+	} else {
+		c.innerHTML += brand;
+		c.innerHTML += welcome;
+
+		a.loadArguments(c);
+		a.addArgument(c);
+	}
+
+	const submitArgumentButton = document.getElementById('submit');
+	submitArgumentButton.addEventListener('click', function() {
+		const [i, t] = document.querySelectorAll('input');
+		a.storeArgument(i.value, t.value);
+		document.getElementById('activeArgument').remove();
+		a.loadArguments(c);
+		a.addArgument(c);
+	});
+
+	return document;
 }
 
 module.exports = { init };
--- src/arguments.js
+++ src/arguments.js
@@ -1,6 +1,40 @@
-function storeArgument(i, t) {
-  const args = [i, t];
-  localStorage.setItem(`${i}-${t}`, JSON.stringify(args));
+const startArgument = `<div id="activeArgument" class="argument">If <input name="if" class="ifInput" placeholder="x is true">, then <input name="then" class="thenInput" placeholder="y is true">. <button id="submit" class="submitArgument">+</button></div>`;
+
+function argTemplate(a) {
+	const [i, t] = Object.values(a)[0];
+	return `<div id="${Object.keys(a)}" class="argument">If ${i}, then ${t}.</div>`;
 }
 
-module.exports = { storeArgument }
+function loadArguments(container) {
+	var arguments = JSON.parse(localStorage.getItem('arguments'));
+	var serialized = arguments.map(e => {
+		return argTemplate(e);
+	});
+
+	container.innerHTML += serialized;
+}
+
+function initStorage() {
+	const currentStore = localStorage.getItem('arguments');
+	if(currentStore === null) {
+		localStorage.setItem('arguments', JSON.stringify([]));
+	}
+}
+
+function storeArgument(i, t) {
+	const args = [i, t];
+	const key = `${i}-${t}`;
+	var o = {};
+	o[key] = args;
+
+	var storedArguments = JSON.parse(localStorage.getItem('arguments'));
+	storedArguments.push(o);
+
+	localStorage.setItem('arguments', JSON.stringify(storedArguments));
+}
+
+function addArgument(container) {
+	container.innerHTML += startArgument;
+}
+
+module.exports = { storeArgument, addArgument, loadArguments, initStorage }
