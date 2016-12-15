8c405ee9c1a65738554370ee65b051e4ff3360fc
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-12-14 21:42:22 -0500

    style loader

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 var containerId = 'appContainer';
 var i18nService = require('./i18n_service.js');
 
+const brand = `<h1>${i18nService.t('brand.appName')}</h1>`;
+const welcome = `<p id="welcome" class="welcome">${i18nService.t('welcome to my app')}!</p>`;
+
 function init() {
   var c = document.getElementById(containerId);
     if(c === null) {
       console.log(i18nService.t('errors.noRoot'));
     } else {
-      var brand = `<h1>${i18nService.t('brand.appName')}</h1>`;
       c.innerHTML += brand;
-      var html = `<div id="welcome" class="welcome">${i18nService.t('welcome to my app')}</div>`;
-      c.innerHTML += html;
+      c.innerHTML += welcome;
     }
   return document;
 }
