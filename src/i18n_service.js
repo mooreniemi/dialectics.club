f44c2c6f5bfa758f18e2bef09df23a7f08bc5500
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-12-14 19:25:27 -0500

    cache i18n

--- src/i18n_service.js
+++ src/i18n_service.js
@@ -1,16 +1,19 @@
 // http://i18next.com/docs/
 var i18next = require('i18next');
+var i18nCache = require('i18next-localstorage-cache');
 
-module.exports = i18next.init({
-  lng: 'en',
-  resources: {
-    en: {
-      translation: {
-        "welcome to my app": "welcome to my app",
-        "errors": {
-          "no root": "lacked container to root in"
+module.exports = i18next.
+  use(i18nCache).
+  init({
+    lng: 'en',
+    resources: {
+      en: {
+        translation: {
+          "welcome to my app": "welcome to my app",
+          "errors": {
+            "no root": "lacked container to root in"
+          }
         }
       }
     }
-  }
-});
+  });
