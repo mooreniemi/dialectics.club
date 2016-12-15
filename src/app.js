05ecb82b527ecb27e4433f0788de1b988d263732
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-12-14 21:47:39 -0500

    syntax mostly

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-var containerId = 'appContainer';
+const containerId = 'appContainer';
 var i18nService = require('./i18n_service.js');
 
-const brand = `<h1>${i18nService.t('brand.appName')}</h1>`;
+const brand = `<h1 class="brandName">${i18nService.t('brand.appName')}</h1>`;
 const welcome = `<p id="welcome" class="welcome">${i18nService.t('welcome to my app')}!</p>`;
 
 function init() {
-  var c = document.getElementById(containerId);
+  const c = document.getElementById(containerId);
     if(c === null) {
       console.log(i18nService.t('errors.noRoot'));
     } else {
